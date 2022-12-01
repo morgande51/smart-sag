2b8e71590fc44a0245c119fc08bc3978694a322b
Darnell Morgan <morgande51@gmail.com>
2022-12-01 14:47:02 -0500

    dedicated endpoints for me ride types

--- smart-sag-service/src/main/java/org/nge/smartsag/service/Me.java
+++ smart-sag-service/src/main/java/org/nge/smartsag/service/Me.java
@@ -1,8 +1,5 @@
 package org.nge.smartsag.service;
 
-import java.util.List;
-
-import org.nge.smartsag.domain.Ride;
 import org.nge.smartsag.domain.User;
 
 import lombok.Getter;
@@ -17,25 +14,7 @@ public class Me {
 	@Setter
 	private ActiveSAGRequest activeSAGRequest;
 	
-	private Long[] activeSupportRideIds = new Long[]{};
-	
-	private Long[] activeMarshalRideIds = new Long[]{};
-	
-	private Long[] activeAdminRideIds = new Long[]{};
-	
 	public Me(User account) {
 		this.account = account;
-	}
-
-	public void setSupportRides(List<Ride> rides) {
-		activeSupportRideIds = rides.stream().map(Ride::getId).toArray(i -> new Long[i]);
-	}
-	
-	public void setMarshalRides(List<Ride> rides) {
-		activeMarshalRideIds = rides.stream().map(Ride::getId).toArray(i -> new Long[i]);
-	}
-	
-	public void setAdminRides(List<Ride> rides) {
-		activeAdminRideIds = rides.stream().map(Ride::getId).toArray(i -> new Long[i]);
 	}
 }
--- smart-sag-service/src/main/java/org/nge/smartsag/service/UserService.java
+++ smart-sag-service/src/main/java/org/nge/smartsag/service/UserService.java
@@ -7,8 +7,10 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 import org.jboss.logging.Logger;
@@ -28,6 +30,8 @@ import org.nge.smartsag.service.ActiveSAGRequest.ContextType;
 public class UserService extends ContextedUserSupport {
 	
 	private static final Logger log = Logger.getLogger(UserService.class);
+	
+	private static enum RIDE_SUPPORT_TYPES {MARSHAL, HOST, SUPPORT};
 	
 	@Inject
 	UserDao userDao;
@@ -65,9 +69,6 @@ public class UserService extends ContextedUserSupport {
 	public Me whoAmiI() {
 		User me = getAuthenticatedUser();
 		List<SAGRequest> sagRequests = sagDao.findRequestForUser(me.getId(), true);
-		List<Ride> supportRides = rideDao.findRidesForSAGSupport(me.getId(), true);
-		List<Ride> marshalRides = rideDao.findRidesForMarshal(me.getId(), true);
-		List<Ride> adminRides = rideDao.findRidesForAdmin(me.getId(), true);
 		
 		Me response = new Me(me);
 		if (sagRequests != null && !sagRequests.isEmpty()) {
@@ -82,19 +83,33 @@ public class UserService extends ContextedUserSupport {
 			response.setActiveSAGRequest(new ActiveSAGRequest(context, sagRequest.getId()));
 		}
 		
-		if (supportRides != null && !supportRides.isEmpty()) {
-			response.setSupportRides(supportRides);
-		}
-		
-		if (marshalRides != null && !marshalRides.isEmpty()) {
-			response.setMarshalRides(marshalRides);
-		}
-		
-		if (adminRides != null && !adminRides.isEmpty()) {
-			response.setAdminRides(adminRides);
-		}
-		
 		log.debugf("Context Response: %s", response);
 		return response;
+	}
+	
+	@Path("/me/{rideType}")
+	@GET
+	public ChildrenReference<Long, Ride> getMyRides(
+			@PathParam("rideType") String rt,
+			@QueryParam("active") @DefaultValue("true") boolean active) 
+	{
+		log.debugf("We have the following ride type: %s", rt);
+		User me = getAuthenticatedUser();
+		List<Ride> rides;
+		
+		switch (RIDE_SUPPORT_TYPES.valueOf(rt)) {
+			case HOST:
+				rides = rideDao.findRidesForAdmin(me.getId(), active);
+				break;
+				
+			case MARSHAL:
+				rides = rideDao.findRidesForMarshal(me.getId(), active);
+				break;
+				
+			default:
+				rides = rideDao.findRidesForSAGSupport(me.getId(), active);
+		}
+		
+		return new ChildrenReference<>(rides, "rides");
 	}
 }
